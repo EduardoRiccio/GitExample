7f150e2ac41fe211b3e35ef3131fed4488f85cd9
Eduardo Riccio <eduardo.riccio@gmail.com>
2017-03-08 16:28:04 -0300

    Initial Commit.

---
+++ app/src/main/java/com/example/eduar/mygittest/GitActivity.java
@@ -0,0 +1,13 @@
+package com.example.eduar.mygittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git);
+    }
+}
